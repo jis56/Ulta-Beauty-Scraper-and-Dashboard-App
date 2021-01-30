f1d1fafdc1bba529d9086d5ac765fc75a8e04ba3
Jingwen Sun <viviensun2009@gmail.com>
2021-01-30 13:11:41 -0500

    sql push, flask init

---
+++ table.sql
@@ -0,0 +1,7 @@
+create table ulta(
+	Brand varchar not null,
+	Product varchar not null,
+	Product_Type varchar not null,
+	Price money not null,
+	Rating varchar not null,
+	Image_URL varchar not null)
