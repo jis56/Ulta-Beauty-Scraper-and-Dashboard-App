c6fdbb4913230fa7e34d5566e9488260674b791d
Jingwen Sun <viviensun2009@gmail.com>
2021-02-04 20:00:19 -0500

    js

--- app.py
+++ app.py
@@ -13,15 +13,54 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/ulta_db")
 # Route to render data from Mongo
 @app.route("/")
 def home():
-   ulta_data = []
-   #Find data from the mongo database
-   for data in mongo.db.ulta.find({}, {"_id":False}):
-       ulta_data.append(data)
-   #dumped_data = [json_util.dumps(data) for data in ulta_data]
-   #cleaned_data = [json.loads(data) for data in dumped_data]
+    ulta_data = []
 
-   #return jsonify(cleaned_data)
-   return render_template("index.html", ulta_data = ulta_data)
+    #Find data from the mongo database
+    for data in mongo.db.ulta.find({}, {"_id":False}):
+        ulta_data.append(data)
+    #dumped_data = [json_util.dumps(data) for data in ulta_data]
+    #cleaned_data = [json.loads(data) for data in dumped_data]
+
+    #return jsonify(cleaned_data)
+    return render_template("index.html", ulta_data = ulta_data)
+
+@app.route("/foundation")
+def foundation():
+    foundation_data = []
+
+    #Find data from the mongo database
+    for data in mongo.db.ulta.find({"product_type":"Foundation"}, {"_id":False}):
+        foundation_data.append(data)
+
+    return foundation_data
+
+@app.route("/eyeshadow")
+def eyeshadow():
+    eyeshadow_data = []
+
+    #Find data from the mongo database
+    for data in mongo.db.ulta.find({"product_type":"Eyeshadow"}, {"_id":False}):
+        eyeshadow_data.append(data)
+    return render_template("index.html", eyeshadow_data = eyeshadow_data)
+
+@app.route("/blush")
+def blush():
+    blush_data = []
+
+    #Find data from the mongo database
+    for data in mongo.db.ulta.find({"product_type":"Blush"}, {"_id":False}):
+        blush_data.append(data)
+
+    return render_template("index.html", blush_data = blush_data)
+
+@app.route("/lipstick")
+def lipstick():
+    lipstick_data = []
+
+    #Find data from the mongo database
+    for data in mongo.db.ulta.find({"product_type":"Lipstick"}, {"_id":False}):
+        lipstick_data.append(data)
+    return render_template("index.html", lipstick_data = lipstick_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/app.js
+++ static/app.js
@@ -26,102 +26,116 @@
     plotFunctions();
   }; */
 
-var svgWidth = 960;
-var svgHeight = 500;
+  function createChart(productID) {
+      $.getJSON('/'+productID, {
+      }, function(data) {
+        console.log(data);
+      })
+      .fail(function() { alert("error"); })
+      .always(function() { alert("complete"); });
+      return false;
+  }; 
 
-var margin = {
-  top: 20,
-  right: 40,
-  bottom: 60,
-  left: 100
-};
-  
+  //console.log(ulta_data)
+  var svgWidth = 960;
+  var svgHeight = 500;
 
-// Calculate chart width and height
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
+  var margin = {
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 100
+  };
+    
+  // Calculate chart width and height
+  var width = svgWidth - margin.left - margin.right;
+  var height = svgHeight - margin.top - margin.bottom;
 
-// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select(".chart")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+  // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+  var svg = d3.select(".chart")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
 
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// Import Data
+  // Import Data
 
-// Step 2: Create scale functions
-// ==============================
-var xLinearScale = d3.scaleLinear()
-  .domain([0, d3.max(ulta_data, d => d.price)])
-  .range([0, width]);
+  // Step 2: Create scale functions
+  // ==============================
+  var xLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(ulta_data, d => d.price)])
+    .range([0, width]);
 
-var yLinearScale = d3.scaleLinear()
-  .domain([1, d3.max(ulta_data, d => d.rating)])
-  .range([height, 0]);
+  var yLinearScale = d3.scaleLinear()
+    .domain([1, d3.max(ulta_data, d => d.rating)])
+    .range([height, 0]);
 
-// Step 3: Create axis functions
-// ==============================
-var bottomAxis = d3.axisBottom(xLinearScale);
-var leftAxis = d3.axisLeft(yLinearScale);
+  // Step 3: Create axis functions
+  // ==============================
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
 
-// Step 4: Append Axes to the chart
-// ==============================
-chartGroup.append("g")
-  .attr("transform", `translate(0, ${height})`)
-  .call(bottomAxis);
+  // Step 4: Append Axes to the chart
+  // ==============================
+  chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
 
-chartGroup.append("g")
-  .call(leftAxis);
+  chartGroup.append("g")
+    .call(leftAxis);
 
-// Step 5: Create Circles
-// ==============================
-var circlesGroup = chartGroup.selectAll("circle")
-.data(ulta_data)
-.enter()
-.append("circle")
-.attr("cx", d => xLinearScale(d.price))
-.attr("cy", d => yLinearScale(d.rating))
-.attr("r", "10")
-.attr("fill", "pink")
-.attr('stroke', '#e3e3e3')
-.attr("opacity", "1");
+  // Step 5: Create Circles
+  // ==============================
+  var circlesGroup = chartGroup.selectAll("circle")
+  .data(ulta_data)
+  .enter()
+  .append("circle")
+  .attr("cx", d => xLinearScale(d.price))
+  .attr("cy", d => yLinearScale(d.rating))
+  .attr("r", "10")
+  .attr("fill", "pink")
+  .attr('stroke', '#e3e3e3')
+  .attr("opacity", "1");
 
-// Step 6: Initialize tool tip
-// ==============================
-var toolTip = d3.tip()
-  .attr("class", "tooltip")
-  .offset([80, -60])
-  .html(function(d) {
-    return (`Product:${d.product}<br>Price: ${d.price}<br>Rating: ${d.rating}`);
-  });
+  // Step 6: Initialize tool tip
+  // ==============================
+  var toolTip = d3.tip()
+    .attr("class", "tooltip")
+    .offset([80, -60])
+    .html(function(d) {
+      return (`Product:${d.product}<br>Price: ${d.price}<br>Rating: ${d.rating}`);
+    });
 
-// Step 7: Create tooltip in the chart
-// ==============================
-chartGroup.call(toolTip);
+  // Step 7: Create tooltip in the chart
+  // ==============================
+  chartGroup.call(toolTip);
 
-// Step 8: Create event listeners to display and hide the tooltip
-// ==============================
+  // Step 8: Create event listeners to display and hide the tooltip
+  // ==============================
+  circlesGroup.on("click", function(data) {
+    toolTip.show(data, this);
+  })
+    // onmouseout event
+    .on("mouseout", function(data, index) {
+      toolTip.hide(data);
+    });
+
+  // Create axes labels
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left + 40)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("Rating");
+
+  chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    .attr("class", "axisText")
+    .text("Price");
+
 circlesGroup.on("click", function(data) {
   toolTip.show(data, this);
 })
-  // onmouseout event
-  .on("mouseout", function(data, index) {
-    toolTip.hide(data);
-  });
-
-// Create axes labels
-chartGroup.append("text")
-  .attr("transform", "rotate(-90)")
-  .attr("y", 0 - margin.left + 40)
-  .attr("x", 0 - (height / 2))
-  .attr("dy", "1em")
-  .attr("class", "axisText")
-  .text("Rating");
-
-chartGroup.append("text")
-  .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-  .attr("class", "axisText")
-  .text("Price");
